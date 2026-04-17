da328ce67efcbc59bc3fea05f74a36b61d014fb4
VyktorM23 <128824667+VyktorM23@users.noreply.github.com>
2026-04-17 00:35:25 -0400

    Add files via upload

---
+++ MiprimerPWA/sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'hospital-pwa-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './manifest.json',
+    './icon-72x72.png',
+    './icon-96x96.png',
+    './icon-128x128.png',
+    './icon-144x144.png',
+    './icon-152x152.png',
+    './icon-192x192.png',
+    './icon-384x384.png',
+    './icon-512x512.png',
+    'https://cdn.jsdelivr.net/npm/jsqr@1.4.0/dist/jsQR.min.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
