c96998360767a1f6d5098a466f84ea61c93b4494
VyktorM23 <128824667+VyktorM23@users.noreply.github.com>
2026-05-23 11:53:47 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,14 +5,14 @@ const urlsToCache = [
     './style.css',
     './app.js',
     './manifest.json',
-    './icon-72x72.png',
-    './icon-96x96.png',
-    './icon-128x128.png',
-    './icon-144x144.png',
-    './icon-152x152.png',
-    './icon-192x192.png',
-    './icon-384x384.png',
-    './icon-512x512.png',
+    '.icons/hospital-logo.png',
+    '.icons/hospital-logo.png',
+    '.icons/hospital-logo.png',
+    '.icons/hospital-logo.png',
+    '.icons/hospital-logo.png',
+    '.icons/hospital-logo.png',
+    '.icons/hospital-logo.png',
+    '.icons/hospital-logo.png',
     'https://cdn.jsdelivr.net/npm/jsqr@1.4.0/dist/jsQR.min.js'
 ];
 
